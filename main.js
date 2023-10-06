58085585dafc06bb1f61db0e79668e254033b18e
Chris Connelly <57345410+teamallnighter@users.noreply.github.com>
2023-10-06 06:54:22 -0300

    INIT
    INITING

---
+++ main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 550,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: false, // Disable Node.js integration for security reasons
+      contextIsolation: true, // Enable context isolation
+    },
+  });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true,
+    })
+  );
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
